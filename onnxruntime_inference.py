025a298df98b959f5c016b8f325472a466430c81
nikita <noivanov1@gmail.com>
2022-02-08 01:16:56 +0300

    refactor onnxruntime_inference.py, change mxnet lib in future to run inference clearly

--- onnxruntime_inference.py
+++ onnxruntime_inference.py
@@ -1,29 +1,50 @@
 import onnxruntime
 import onnx
 import numpy as np
-#import cv2
-import os
+import config
+
 from PIL import Image
 
-model_path = 'model_onnx/converted_model.onnx'
-onnx_model = onnx.load(model_path)
 
-content = onnx_model.SerializeToString()
-sess = onnxruntime.InferenceSession(content)
-image = 'photo.bmp'
-image = Image.open(image)
-img = np.array(image)
+def image_preprocessing(image_name: str) -> np.ndarray:
+    """
+    Preprocessing the image for model
+    """
+    image = Image.open(image_name)
+    img = np.array(image)
+    img = np.swapaxes(img, 0, 2)
+    img = np.swapaxes(img, 1, 2)  # change to (c, h,w) order
+    img = np.expand_dims(img, axis=0).astype(np.float32)
+    return img
 
-img = np.swapaxes(img, 0, 2)
-img = np.swapaxes(img, 1, 2)  # change to (c, h,w) order
-img = np.expand_dims(img, axis=0).astype(np.float32)
 
-print(img.shape)
-print(type(img))
-feed1 = {sess.get_inputs()[0].name: img}
-out = sess.run(None, feed1)[0][0]
-print(out)
+def get_model (onnx_model_name: str):
+    return onnx.load(onnx_model_name)
 
-with open('onnx.txt', "w") as file:
-    for i in range(len(out)):
-        file.write(str(out[i]) + '\n')
+
+def model_output(loaded_model, input_image):
+    content = loaded_model.SerializeToString()
+    sess = onnxruntime.InferenceSession(content)
+    feed1 = {sess.get_inputs()[0].name: input_image}
+    return sess.run(None, feed1)[0][0]
+
+
+def write_output(file_name: str, model_out: np.ndarray):
+    """
+    Write embedding to .txt file
+    """
+    with open(file_name, 'w') as out:
+        for i in range(len(model_out)):
+            out.write(str(model_out[i]) + '\n')
+
+
+def main():
+    loaded_onnx_model = get_model(config.onnx_model_name)
+    input_image = image_preprocessing(config.image_name)
+    model_out = model_output(loaded_onnx_model, input_image)
+    write_output(config.onnx_output_file, model_out)
+    print('Done.')
+
+
+if __name__ == "__main__":
+    main()
