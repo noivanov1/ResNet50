988a2cccb5606804b493b2c871d2be77ab6a94b6
nikita <noivanov1@gmail.com>
2022-02-09 21:54:30 +0300

    edit config conversion_input_size to input_shape relation

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 # General
 mxnet_model_prefix = 'model_mxnet/model'
 image_name = 'photo.bmp'
-input_size = (112, 112)
+input_shape = 1,3,112,112
 onnx_model_name = 'model_onnx/converted_model.onnx'
 
 
 # Conversion MXNet to ONNX
 mxnet2onnx_log = 'mxnet2onnx_log.txt'
-conversion_input_size = 1,3,112,112
 
 
 # MXNet inference
