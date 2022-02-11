fa30e1e42231821cef11b820836e700749fc795d
nikita <noivanov1@gmail.com>
2022-02-11 11:13:22 +0300

    refactor --model_output in inference args

--- args_parser.py
+++ args_parser.py
@@ -8,5 +8,11 @@ parser.add_argument("--epoch", default=config.epoch, type=int, help="epoch of th
 parser.add_argument("--input_image", default=config.image_name, type=str, help="name of the input image")
 parser.add_argument("--input_shape", default=[config.input_shape[2], config.input_shape[3]],
                     help="input shape of the model")
-parser.add_argument("--model_output", default=config.mxnet_output_file, type=str, help="write embedding to file")
+parser.add_argument("--mxnet_model_output", default=config.mxnet_output_file, type=str, help="write embedding to file")
+parser.add_argument("--onnx_model", default=config.onnx_model_name, type=str, help="name of converted ONNX model")
+parser.add_argument("--onnxruntime_model_output", default=config.onnxruntime_output_file, type=str, help="write embedding to file")
 args = parser.parse_args()
+
+
+
+
--- mxnet_inference.py
+++ mxnet_inference.py
@@ -11,7 +11,7 @@ def main():
     image = preprocess_image(args_parser.args.input_image, literal_eval(str(args_parser.args.input_shape)))
     model = load_model_mxnet(ctx, args_parser.args.prefix, args_parser.args.epoch, image)
     model_out = get_model_output_onnx_mxnet(model, image)
-    write_output(args_parser.args.model_output, model_out)
+    write_output(args_parser.args.mxnet_model_output, model_out)
     print(f'Done! Check {config.mxnet_output_file}')
 
 
